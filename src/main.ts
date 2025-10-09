e6344c0d196faa60a356a2911ac306629f27d97f
Kaitlyn Eng <123908591+kaitlyn-png@users.noreply.github.com>
2025-10-09 20:07:22 +0000

    cleaned up files, added 2 images, added a clickable button (step 1 complete)”

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
-import cookieURL from "./cookie.png";
+import axolotlURL from "./axolotl.png";
 import "./style.css";
+import wormURL from "./worm.png";
+
+let counter: number = 0;
 
 document.body.innerHTML = `
-  <p>Cookie asset: <img src="${cookieURL}" class="icon" /></p>
+  <h1>Help me feed my axolotl so that he can grow big and strong and become president of the galaxy</h1>
+  <p>Fed <span id="counter">0</span> times</p>
+
+  <button id="increment">
+    <p id = "feed"> FEED 
+      <img id= "worm" src=${wormURL} alt="worm icon">
+    </p>
+  </button>
+  
+  <img id= "axolotl" src=${axolotlURL} alt="axolotl icon">
 `;
 
-console.log("helloo");
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter += 1;
+  counterElement.textContent = counter.toString();
+  console.log("Times fed: ", button, counterElement, counter);
+});
+
+console.log("RUNNING");
